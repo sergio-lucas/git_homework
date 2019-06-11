4fcd0614ebabe4b70704fd33c1b0b850846163ad
Sergey <lukjanchuks@gmail.com>
2019-06-11 12:09:32 +0300

    merge feature/answers

---
+++ js/answers.js
@@ -0,0 +1,19 @@
+function saveAnswer(question, answerText) {
+  const { correctOption } = question;
+
+  const isCorrect = correctOption === null ? null : correctOption === answerText;
+  return { question, answerText, isCorrect }
+}
+
+function askQuestion(question) {
+  let text = question.text;
+
+  if(question.options) {
+    text += '\n';
+    text = question.options.map(opt => '- ' + opt).join('\n')
+  }
+
+  const answerText = prompt(text);
+
+  return saveAnswer(question, answerText);
+}
