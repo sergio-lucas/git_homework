125ba22105d243bf91c35dd86069c363e770d904
Sergey <lukjanchuks@gmail.com>
2019-06-11 12:08:03 +0300

    merge feature/quiz

---
+++ js/quiz.js
@@ -0,0 +1,27 @@
+function createQuiz(questions) {
+  return {
+    questions,
+    answers: null,
+  }
+}
+
+function runQuiz(quiz) {
+  const { questions } = quiz;
+
+  quiz.answers = questions.map(askQuestion);
+
+  return quiz;
+} 
+
+const questions = [
+  createQuestion('Name', 'What\'s your name'),
+  createQuestion('Company', 'What\'s your company'),
+  createQuestion('Salary', 'Wanna get paid?', ['Yes', 'No'], 'No'),
+]
+
+const quiz = createQuiz(questions);
+const answeredQuiz = runQuiz(quiz);
+
+console.log(
+  answeredQuiz
+);
